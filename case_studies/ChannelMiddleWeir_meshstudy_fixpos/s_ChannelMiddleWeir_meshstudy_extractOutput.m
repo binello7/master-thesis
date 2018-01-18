0bd7289acfa55f2ae62c00f8e348eca2ef89bb17
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-01-18 20:35:35 +0100

    add scripts for fixpos and fixshape case studies

--- case_studies/ChannelMiddleWeir_meshstudy_fixpos/s_ChannelMiddleWeir_meshstudy_extractOutput.m
+++ case_studies/ChannelMiddleWeir_meshstudy_fixpos/s_ChannelMiddleWeir_meshstudy_extractOutput.m
@@ -18,28 +18,29 @@
 
 pkg load fswof2d
 
-## Global parameters
-#
-dataFolder  = 'data';
-studyName   = 'ChannelMiddleWeir_meshstudy_2';
 
 ## Variables to save to the outputfile
 #
-vars = {'XX', 'YY', 'ZZ', 'HH', 'HHz', 'FFr', 'VVn', 'nExp', ...
-       'Nx', 'Ny', 'Ly', 'metadata'};
+vars = {'Y', 'Z', 'H', 'middleweir_head', 'HZ', 'H_max', 'Y_max', 'nExp', ...
+       'Nx', 'Ny', 'dx', 'dy', 'B', 'Ly', 'metadata'};
+
 metadata.XX = 'Matrix of the x-nodes coordinates';
 metadata.YY = 'Matrix of the y-nodes coordinates';
 metadata.ZZ = 'Matrix of the z-nodes coordinates';
 
 ## Generate path of needed files
-studyFolder          = fullfile (dataFolder, studyName);
-inputsFolder         = fullfile (studyFolder, 'Inputs');
-load (fullfile (studyFolder, 'input_variables.dat'));
-nExp                 = size (ls ('-d', fullfile (studyFolder, 'Outputs_*')))(1);
+#
+inputsFolder    = 'Inputs';
+nExp            = size (ls ('-d', 'Outputs_*'))(1);
+load 'input_variables.dat';
 
 ## Extract data from files and convert them to 'octave format'
+#
+weir_mes_pos = (wTop_l + wTop_r) / 2;
+middleweir_head         = zeros (1, nExp);
+
 for i = 1:nExp
-  outputsFolder = fullfile (studyFolder, sprintf ('Outputs_%02d', i));
+  outputsFolder = sprintf ('Outputs_%02d', i);
   loadfile      = @(s) load (fullfile (outputsFolder, s));
   data_init     = loadfile ('huz_initial.dat');
   data_evl      = loadfile ('huz_evolution.dat');
@@ -47,33 +48,37 @@ for i = 1:nExp
   params{i}     = read_params (paramsfile);
   Nx(i) = params{i}.Nxcell;
   Ny(i) = params{i}.Nycell;
+  B     = params{1}.L;
   Ly    = params{1}.l;
-  X{i}  = data_init(:,1);
-  Y{i}  = data_init(:,2);
-  Z{i}  = data_init(:,7);
-  H{i}  = data_evl(:,3);
-  Hz{i} = data_evl(:,6);
-  Fr{i} = data_evl(:,9);
-  Vn{i} = data_evl(:,8);
+  Y{i}  = data_init(1:Ny(i),2);
+  Z{i}  = data_init(1:Ny(i),7);
   clear data_init;
+  h{i}  = data_evl(:,3);
   clear data_evl;
-  
-  [XX{i} YY{i} ZZ{i}] = dataconvert ('octave', [Nx(i) Ny(i)], X{i}, Y{i}, Z{i});
-  
+
   nstates = params{i}.nbtimes;
+
   for t = 1:nstates
     idx_1 = 1 + (t - 1) * Nx(i) * Ny(i);
     idx_2 = t * Nx(i) * Ny(i);
     span = idx_1:idx_2;
-
-    [HH{i}(:,:,t) HHz{i}(:,:,t) FFr{i}(:,:,t) VVn{i}(:,:,t)] = ...
-    dataconvert ('octave', [Nx(i) Ny(i)], ...
-    H{i}(span), Hz{i}(span), Fr{i}(span), Vn{i}(span));
+    HH{i}(:,:,t) = dataconvert ('octave', [Nx(i) Ny(i)], h{i}(span));
   endfor
 
+  H{i} = mean (HH{i}(:, round (end/2), 100:end), 3);
+  [H_max(i), max_idx(i)] = max (H{i});
+  HZ{i} = H{i} + Z{i};
+  middleweir_head(i) = interp1 (Y{i}, H{i}, weir_mes_pos);
 endfor
 
-clear X Y Z H Hz Fr Vn
-save (fullfile (studyFolder, 'extracted_data.dat'), vars{:});
-save (fullfile (studyFolder, 'metadata.dat'), 'metadata');
+dx = B ./ Nx;
+dy = Ly ./ Ny;
+
+Y_max = (max_idx - 1) .* dy .+ 0.5 * dy;
+
+clear h HH idx_1 idx_2 span params paramsfile
+
+
+save ('extracted_data.dat', vars{:});
+save ('metadata.dat', 'metadata');
 
