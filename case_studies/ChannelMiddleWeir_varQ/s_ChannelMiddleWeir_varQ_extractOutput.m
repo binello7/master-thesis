f796298b97fce2c7640e0979e67ef27925149bd6
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-01-22 15:22:37 +0100

    s_ChannelMiddleWeir_varQ_extractOutput.m: adapt script for looping through Qin

--- case_studies/ChannelMiddleWeir_varQ/s_ChannelMiddleWeir_varQ_extractOutput.m
+++ case_studies/ChannelMiddleWeir_varQ/s_ChannelMiddleWeir_varQ_extractOutput.m
@@ -14,71 +14,78 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 ## Author: Sebastiano Rusca <sebastiano.rusca@gmail.com>
-## Created: 2018-01-09
+## Created: 2018-01-21
 
 pkg load fswof2d
 
 
 ## Variables to save to the outputfile
 #
-vars = {'Y', 'Z', 'H', 'middleweir_head', 'HZ', 'H_max', 'Y_max', 'nExp', ...
-       'Nx', 'Ny', 'dx', 'dy', 'B', 'Ly', 'metadata'};
+vars = {'Y', 'Z', 'H', 'h0', 'v0', 'weircenter_head', 'weir_height', 'HZ', 'H_max', 'Y_max', 'nExp', ...
+       'Nx', 'Ny', 'dy', 'Ly', 'B'};
 
-metadata.XX = 'Matrix of the x-nodes coordinates';
-metadata.YY = 'Matrix of the y-nodes coordinates';
-metadata.ZZ = 'Matrix of the z-nodes coordinates';
 
-## Generate path of needed files
+## Generate path of needed files and load variables
 #
 inputsFolder    = 'Inputs';
-nExp            = size (ls ('-d', 'Outputs_*'))(1);
+topo    = load (fullfile (inputsFolder, 'topography.dat'));
+params1 = read_params (fullfile (inputsFolder, 'parameters_01.txt'));
 load 'input_variables.dat';
 
-## Extract data from files and convert them to 'octave format'
+## Extract parameters common to all studies
 #
-weir_mes_pos = (wTop_l + wTop_r) / 2;
-middleweir_head         = zeros (1, nExp);
+Nx      = params1.Nxcell;
+Ny      = params1.Nycell;
+Ly      = params1.l;
+B       = params1.L;
+nstates = params1.nbtimes;
 
+## Extract channel longitudinal profile and weir position
+#
+y = topo(:,2);
+z = topo(:,3);
+
+Y = y(1:Ny);
+Z = z(1:Ny);
+
+weir_center     = (pweir.breaks(3) + pweir.breaks(4)) / 2;
+
+## Create variables to extract
+#
+nExp            = size (ls ('-d', 'Outputs_*'))(1);
+weircenter_head = zeros (1, nExp);
+H_max           = zeros (1, nExp);
+
+## Extract results from each experiment
+#
 for i = 1:nExp
   outputsFolder = sprintf ('Outputs_%02d', i);
   loadfile      = @(s) load (fullfile (outputsFolder, s));
-  data_init     = loadfile ('huz_initial.dat');
   data_evl      = loadfile ('huz_evolution.dat');
-  paramsfile    = fullfile (inputsFolder, sprintf ('parameters_%02d.dat', i));
-  params{i}     = read_params (paramsfile);
-  Nx(i) = params{i}.Nxcell;
-  Ny(i) = params{i}.Nycell;
-  B     = params{1}.L;
-  Ly    = params{1}.l;
-  Y{i}  = data_init(1:Ny(i),2);
-  Z{i}  = data_init(1:Ny(i),7);
-  clear data_init;
-  h{i}  = data_evl(:,3);
+  h(:,i)        = data_evl(:,3);
+  v(:,i)        = data_evl(:,5);
   clear data_evl;
 
-  nstates = params{i}.nbtimes;
-
   for t = 1:nstates
-    idx_1 = 1 + (t - 1) * Nx(i) * Ny(i);
-    idx_2 = t * Nx(i) * Ny(i);
+    idx_1 = 1 + (t - 1) * Nx * Ny;
+    idx_2 = t * Nx * Ny;
     span = idx_1:idx_2;
-    HH{i}(:,:,t) = dataconvert ('octave', [Nx(i) Ny(i)], h{i}(span));
+    [HH{i}(:,:,t) VV{i}(:,:,t)] = dataconvert ('octave', [Nx Ny], ...
+                                               h(span,i), v(span,i));
   endfor
 
-  H{i} = mean (HH{i}(:, round (end/2), 100:end), 3);
-  [H_max(i), max_idx(i)] = max (H{i});
-  HZ{i} = H{i} + Z{i};
-  middleweir_head(i) = interp1 (Y{i}, H{i}, weir_mes_pos);
+  H(:,i) = mean (HH{i}(:, round (end/2), 100:end), 3);
+  h0(i)  = mean (mean (HH{i}(240, :, 100:end), 3), 2);
+  v0(i)  = mean (mean (VV{i}(240, :, 100:end), 3), 2);
+  [H_max(i), max_idx(i)] = max (H(:,i));
+  HZ(:,i) = H(:,i) + Z;
+  weircenter_head(i) = interp1 (Y, H(:,i), weir_center);
 endfor
 
-dx = B ./ Nx;
-dy = Ly ./ Ny;
+dy = Ly / Ny;
+Y_max = (max_idx - 0.5) * dy;
 
-Y_max = (max_idx - 1) .* dy .+ 0.5 * dy;
-
-clear h HH idx_1 idx_2 span params paramsfile
-
+clear h HH idx_1 idx_2 span params1
 
 save ('extracted_data.dat', vars{:});
-save ('metadata.dat', 'metadata');
 
