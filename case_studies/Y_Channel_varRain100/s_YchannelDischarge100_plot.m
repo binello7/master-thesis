c37a5638e02bf89a37e786da8691872dce8ecc00
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-02-04 16:40:11 +0100

    s_YChannelDischarge100_plot.m: produce plots for channel and boundary

--- case_studies/Y_Channel_varRain100/s_YchannelDischarge100_plot.m
+++ case_studies/Y_Channel_varRain100/s_YchannelDischarge100_plot.m
@@ -16,7 +16,44 @@
 ## Author: Sebastiano Rusca <sebastiano.rusca@gmail.com>
 ## Created: 2018-02-03
 
+clear all
+close all
+pkg load dataframe
+
+load ('indexes_table.dat');
+load ('qt_bbound.dat');
+load ('qt_bchannel.dat');
+load ('parameters.dat');
+
+nExp = length (qt_bbound);
+#nExp = 6;
 
 
-load ()dlmread('huz_evolution.dat', '\tab', [(i-1)*(Nx*Ny+Nx+3)+6 0 i*(Nx*Ny+Nx+3)+6 0])
+dt = 30;
+
+figure (1)
+for n = 1:nExp
+  t = 1:saved_states(indexes_table(n,3));
+  t = t * dt;
+  leg{n} = sprintf ('Exp #%d', n);
+  hold on
+  plot (t, qt_bbound{n});
+  hold off
+endfor
+
+legend (gca, leg{:});
+
+
+
+figure (2)
+for n = 1:nExp
+  t = 1:saved_states(indexes_table(n,3));
+  t = t * dt;
+  hold on
+  plot (t, qt_bchannel{n});
+  hold off
+endfor
+
+legend (gca, leg{:});
+
 
