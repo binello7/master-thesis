f1d66e3b0a60bb6eb9fa5b8658d48404e9f9c1bb
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-02-05 00:58:03 +0100

    add files

---
+++ mfiles/functions/animation3d.m
@@ -0,0 +1,43 @@
+## Copyright (C) 2018 Sebastiano Rusca
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## Author: Sebastiano Rusca <sebastiano.rusca@gmail.com>
+## Created: 2018-02-02
+
+
+## -*- texinfo -*-
+## @defun {@var{outarg} =} funcname (@var{inarg}, @dots{})
+## @defunx {@var{outarg2} =} funcname (@var{inarg2}, @dots{})
+## Oneliner
+##
+## Explanation usage 1
+##
+## Explanation usage 2
+##
+## @seealso{func1, func2}
+## @end defun
+
+
+function animation3d (XX, YY, VVV, nframes)
+
+h = mesh (XX, YY, VVV(:,:,1), 'edgecolor', 'b');
+grid on;
+tt = title (sprintf ('frame #: %d', 1));
+
+for t = 2:nframes
+  set (h, 'zdata', VVV(:,:,t));
+  set (tt, 'string', sprintf ('frame #: %d', t))
+  pause(0.5);
+endfor
