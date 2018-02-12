1698a475e7dcf258c8b101e63ad4b834f0e71b0d
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-02-12 17:40:10 +0100

    s_ChannelMiddleWeir_varQ_analyzeOutput.m: implement parameters regression with newe method

--- case_studies/ChannelMiddleWeir_varQ/s_ChannelMiddleWeir_varQ_analyzeOutput.m
+++ case_studies/ChannelMiddleWeir_varQ/s_ChannelMiddleWeir_varQ_analyzeOutput.m
@@ -17,100 +17,116 @@
 ## Created: 2018-01-21
 
 close all
+pkg load optim
 
 ## Load the variables
 # load variables if not present in workspace
 load ('input_Q.dat');
 load ('extracted_data.dat');
 
-#i = 1;
-### Remove unusable experiments
-#while i <= size (H)(2)
-#  if weircenter_head(i) < 0.3
-#    weircenter_head(i) = [];
-#    Qin(i)             = [];
-#    H(:,i)             = [];
-#    HZ(:,i)            = [];
-#    H_max(i)           = [];
-#    h0(i)              = [];
-#    v0(i)              = [];
-#  endif
-#  i+=1;
-#endwhile
+i = 1;
+# Remove unusable experiments
+while i <= size (H)(2)
+  if weircenter_head(i) < 0.05
+    weircenter_head(i) = [];
+    Qin(i)             = [];
+    H(:,i)             = [];
+    HZ(:,i)            = [];
+    H_max(i)           = [];
+    h0(i)              = [];
+    v0(i)              = [];
+  endif
+  i+=1;
+endwhile
 
-
+## Input data
 Qin   = abs (Qin);
-Q_reg = linspace (0, 10, 50);
+Qt    = linspace (0, 10, 100);
 H_max = H_max - weir_height;
 h0    = h0 - weir_height;
 
-hweir = @(Q, mu, c) (Q / (2 /3 * mu * sqrt (2*9.81) * B)).^(c);
-iswater=@(z,h,tol) abs(z-h)>tol;
-water=@(z,h,tol=5e-3)ifelse(iswater(z,h,tol), h, NA);
+## Functions
+hweir    = @(Q, p) (Q / (2 /3 * p(1) * sqrt (2*9.81) * B)).^(p(2));
+hweir_ca = @(Q, p) (Q / (2 /3 * p * sqrt (2*9.81) * B)).^(2/3);
+iswater  = @(z,h,tol) abs(z-h)>tol;
+water    = @(z,h,tol=5e-3)ifelse(iswater(z,h,tol), h, NA);
 
 
 ## Perform linear regression
-#
+# method 1
 lQin = (log10 (Qin)).';
 lh0  = (log10 (h0)).';
-
-C1  = 10^(mean (lQin - 3/2 * lh0));
-mu1 = C1 / (2/3 * B * sqrt(2 * 9.81));
-
 n   = length (lh0);
-Lh0 = [ones(n, 1) lh0];
 
-# compute theta
-theta = (pinv(Lh0.'*Lh0))*Lh0.'*lQin;
-C2    = 10^theta(1);
-mu2   = C2 / (2/3 * B * sqrt(2 * 9.81));
-a     = theta(2);
-
-MSE(1) = 1/n * sum ((h0 - hweir(Qin, 0.57, 2/3)).^2);
-MSE(2) = 1/n * sum ((h0 - hweir(Qin, mu1, 2/3)).^2);
-MSE(3) = 1/n * sum ((h0 - hweir(Qin, mu2, 1/a)).^2);
+## C: exponent of the weir equation (usually 2/3 - 3/2, depending if Q(h) or h(Q))l
+#C1  = 10^(mean (lQin - 3/2 * lh0));
+#mu1 = C1 / (2/3 * B * sqrt(2 * 9.81));
 
 
+#Lh0 = [ones(n, 1) lh0];
 
+## compute theta
+#theta = (pinv(Lh0.'*Lh0))*Lh0.'*lQin;
+#C2    = 10^theta(1);
+#mu2   = C2 / (2/3 * B * sqrt(2 * 9.81));
+#a     = theta(2);
+
+## Perform linear regression
+# method 2 - a = 2/3
+[Fca, Pca, CVGca, ITERca, CORPca, COVPca, COVRca, STDRESIDca, Zca, R2ca] = leasqr (Qin.', h0.', 0.6, hweir_ca);
+
+# method 2 - regression on a as well
+[Fra, Pra, CVGra, ITERra, CORPra, COVPra, COVRra, STDRESIDra, Zra, R2ra] = leasqr (Qin.', h0.', [0.6 2/3], hweir);
+
+
+MSE(1) = 1/n * sum ((h0 - hweir (Qin, [0.57; 2/3])).^2);
+MSE(2) = 1/n * sum ((h0 - hweir_ca (Qin, Pca)).^2);
+MSE(3) = 1/n * sum ((h0 - hweir (Qin, Pra)).^2);
+
+
+
+
+
+## Pre-analysis
+# plot of log(data)
 figure (1)
 plot (lh0, lQin, 'o')
-hold on
-plot (log10 (hweir(Qin, mu2, 1/a)), lQin, 'ro')
-hold off
 
-## Generate the plots of the analyzed variables
+
+## Generate the plots
 # plot 1: longitudinal profile for the 25 experiments
-plt_span = 150:250;
+i1 = 150; i2 = 240;
+plt_span_Y = i1:i2;
+plt_span_Z = i2:-1:i1;
+#plt_span_Y = 1:length (Y);
+#plt_span_Z = length (Y):-1:1;
 figure (2)
-plot (Y(plt_span), Z(plt_span), '-k', 'linewidth', 3);
+plot (Y(plt_span_Y), Z(plt_span_Z), '-k', 'linewidth', 3);
 hold on
-plot (Y(plt_span), water (Z(plt_span), HZ(plt_span,:)));
+plot (Y(plt_span_Y), water (Z(plt_span_Z), HZ(plt_span_Z,:)));
 hold off
 axis equal
-title ('Free surface profiles for Q in range 1 - 10 m3/s')
 xlabel ('Ly [m]');
 ylabel ('h [m]')
-annotation ('textarrow', [0.7 0.65], [0.8 0.8], 'string', ' flow direction', ...
+annotation ('textarrow', [0.4 0.45], [0.8 0.8], 'string', ' flow direction', ...
             'color', 'b', 'headlength', 6, 'headwidth', 6)
+
 print ('free_surfaces.eps', '-color');
+print ('free_surfaces.png', '-r300');
 
 # plot 2: Q - h relation for the 25 experiments
 figure (3)
-plot (Qin, h0, 'ko', Q_reg, hweir(Q_reg, 0.57, 2/3), 'b',
-      Q_reg, hweir(Q_reg, mu1, 2/3), 'g', Q_reg, hweir(Q_reg, mu2, 1/a), 'r')
+plot (Qin, h0, 'ko', Qt, hweir (Qt, [0.57, 2/3]), 'b', ...
+      Qt, hweir (Qt, [Pca, 2/3]), 'g', Qt, hweir (Qt, [Pra(1) Pra(2)]), 'r')
 xlabel ('Q [m^3/s]')
 ylabel ('h_{w} [m]')
-legend ('simulated values', '1: trial-error fitting \mu = 0.57',
-        '2: regression on \mu, \mu = 0.553', '3: regression on \mu and a, \mu = 0.560, a =  1.59', 'location', 'northwest')
+
+legend ('simulated values', '1: trial-error fitting \mu = 0.570', ...
+        sprintf ('2: regression on \\mu, \\mu = %0.3f', Pca), ...
+        sprintf ('3: regression on \\mu and a, \\mu = %0.3f, a = %0.2f', Pra(1), 1/Pra(2)), 'location', 'northwest')
+
 print ('points_interpolations.eps', '-color');
-
-# plot XX: logQ - logh relation for the 25 experiments
-#figure (4)
-#loglog (Qin, weircenter_head, 'ro')
-
-
-
-
+print ('points_interpolations.png', '-r300');
 
 
 
