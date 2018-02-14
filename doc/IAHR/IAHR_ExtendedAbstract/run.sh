abd9015ef67e833ec2ba07a64c88b6e3a1137c3a
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-02-14 17:14:17 +0100

    addremove new/old files

---
+++ doc/IAHR/IAHR_ExtendedAbstract/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pdflatex Abstract\ Submission-Rusca\ Sebastiano.tex
+bibtex Abstract\ Submission-Rusca\ Sebastiano
+pdflatex Abstract\ Submission-Rusca\ Sebastiano.tex
+pdflatex Abstract\ Submission-Rusca\ Sebastiano.tex
