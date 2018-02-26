340ee951aeaf0cff2485303b4af675694caa8271
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-02-26 15:28:56 +0100

    s_YchannelDischargeDef_ExtractSVM.m: add file

---
+++ case_studies/Y_Channel_varRainDef/s_YchannelDischargeDef_ExtractSVM.m
@@ -0,0 +1,47 @@
+## Copyright (C) 2018 Sebastiano Rusca
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## Author: Sebastiano Rusca <sebastiano.rusca@gmail.com>
+## Created: 2018-02-26
+
+clear all
+pkg load fswof2d
+
+## Load data
+#
+load ('rain_intensities_svm.dat');
+load ('parameters.dat');
+
+
+n = length (rain_intensities_svm);
+
+j = 1;
+tic
+for i = 1:n
+  for n = 1:saved_states
+    folderSuff    = sprintf ('_svm%02d', i);
+    outputsFolder = strcat ('Outputs', folderSuff);
+    q_temp = dlmread (fullfile (outputsFolder, 'huz_evolution.dat'), '\tab', [(n-1)*(Nx*Ny+Nx+3)+6 11 (n-1)*(Nx*Ny+Nx+3)+5+Nx*Ny 11]);
+    qq_temp = dataconvert ('octave', [Nx Ny], q_temp);
+    qt_bbound_svm(j,n)   = sum (qq_temp(1,:));
+  endfor
+  j +=1;
+endfor
+toc
+
+save ('qt_bbound_svm.dat', 'qt_bbound_svm');
+
+
+
