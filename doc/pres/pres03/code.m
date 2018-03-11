79749b0a6d8e6d0049e6ce003cdbd4a9b987f5b0
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-03-11 09:29:03 +0100

    code.m: make vectors horizontal

--- doc/pres/pres03/code.m
+++ doc/pres/pres03/code.m
@@ -1,7 +1,9 @@
+nQ  = 25; #number of experiments
+Qin = linspace (0.1, 10, nQ); #Qin values [m3/s]
 
+# ------------------------------------------------------------------------------
 
-rain_intensities = [linspace(10, 35, 10)].'; #[mm/h]
-soil_saturations = [linspace(0, 1, 5)].'; #[-]
+rain_intensities = linspace (10, 35, 10); #[mm/h]
+soil_saturations = linspace (0, 1, 5); #[-]
 rain_duration    = 6; #[h]
 sim_duration     = 9; #[h]
-
