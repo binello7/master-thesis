b5fcf78f1070fb6fb4ba6671064f47ef28208e39
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-03-21 11:50:31 +0100

    code.m: make Q-sign negative

--- doc/thesis/code.m
+++ doc/thesis/code.m
@@ -2,4 +2,4 @@
 # generate 25 discharges in range [0.1, 10} m3/s
 
 nQ  = 25; %number of experiments
-Qin = linspace (0.1, 10, nQ); %Qin values [m3/s]
+Qin = linspace (-0.1, -10, nQ); %Qin values [m3/s]
