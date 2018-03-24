9c8dfe30fb7317b7c45bc25a9d6e71a54c668468
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-03-24 21:34:41 +0100

    s_ChannelMiddleWeir_varQ_figures.m: plot water in channel

--- case_studies/ChannelMiddleWeir_varQ/s_ChannelMiddleWeir_varQ_figures.m
+++ case_studies/ChannelMiddleWeir_varQ/s_ChannelMiddleWeir_varQ_figures.m
@@ -22,13 +22,16 @@ pkg load fswof2d
 
 load ('Inputs/topography.dat');
 
+
 bool_plots = {'topography', true};
 
-## Treat topography data
+## Extract useful parameters
 params = read_params ('Inputs/parameters_01.txt');
-Nx = params.Nxcell;
-Ny = params.Nycell;
+Nx      = params.Nxcell;
+Ny      = params.Nycell;
+nstates = params.nbtimes;
 
+## Treat topography data
 X = topography(:,1);
 Y = topography(:,2);
 Z = topography(:,3);
@@ -36,22 +39,44 @@ Z = topography(:,3);
 [X Y Z] = dataconvert ('octave', [Nx Ny], X, Y, Z);
 
 
+
+## Treat water height data
+if ~exist ('H', 'var')
+  load ('Outputs_21/huz_evolution.dat');
+  h = huz_evolution(:,6);
+  H = zeros (Ny, Nx, nstates);
+
+  for t = 1:nstates
+      idx_1 = 1 + (t - 1) * Nx * Ny;
+      idx_2 = t * Nx * Ny;
+      span = idx_1:idx_2;
+      H(:,:,t) = dataconvert ('octave', [Nx Ny], h(span));
+  endfor
+endif
+
+
 if bool_plots{1,2}
   rgb_max = 255;
-  r = 148/rgb_max;
-  g = 145/rgb_max;
-  b = 136/rgb_max;
-  c1 = 1.5 * [r g b];
-  c2 = 0.5 * c1;
-  h = surf (X, Y, Z, 'facecolor', 'w', 'edgecolor', c2, 'linewidth', 0.2);
+  c1 = [96 96 96]/rgb_max;
+  c2 = [192 192 192]/rgb_max;
+  bm = [31 73 145]/rgb_max;
+  tp = 16;
+  h1 = surf (X, Y, Z, 'facecolor', c2, 'edgecolor', c1, 'linewidth', 0.2);
+  hold on
+  h2 = mesh (X, Y, H(:,:,tp), 'edgecolor', bm);
+  set (h2, 'facecolor', 'none');
+  hold off
   set (gca, 'xtick', [0 2 4], 'xticklabel', {'0' '2' '4'}, ...
        'ztick', [0 1 2 3], 'zticklabel', {'0' '1' '2' '3'});
-  axis ([0 4 0 40 0 3])
   axis equal
+  axis ([0 4 0 40 0 max(H(:,:,tp)(:))])
+  axis tight
+  grid off
   view (-32,22)
-  print ('channel.png', '-r600')
+
   xlabel ('x [m]')
   ylabel ('y [m]')
   zlabel ('z [m]')
+  print ('channel.png', '-r600')
 endif
 
