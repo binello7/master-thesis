49746cd05d8e095b74f851c4b8fb39fdc1143a08
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2018-09-17 12:04:27 +0200

    Restructure IAHR Folder and add subfolders 'Presentation' and 'Abstract'

---
+++ doc/IAHR/Presentation/code.m
@@ -0,0 +1,9 @@
+nQ  = 25; #number of experiments
+Qin = linspace (0.1, 10, nQ); #Qin values [m3/s]
+
+# ------------------------------------------------------------------------------
+
+rain_intensities    = linspace (10, 35, 10); #[mm/h]
+initial_saturations = linspace (0, 1, 5); #[-]
+rain_duration       = 6; #[h]
+simulation_duration = 9; #[h]
