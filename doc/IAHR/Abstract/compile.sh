b1ebdb5b1bc9b978c3cea7873d550b662491b9a0
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-04-01 12:21:45 +0200

    Update file -remove unused log files

--- doc/IAHR/Abstract/compile.sh
+++ doc/IAHR/Abstract/compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-pdflatex Abstract\ Submission-Rusca\ Sebastiano.tex
-bibtex Abstract\ Submission-Rusca\ Sebastiano
-pdflatex Abstract\ Submission-Rusca\ Sebastiano.tex
-pdflatex Abstract\ Submission-Rusca\ Sebastiano.tex
+filename="Abstract_SRusca"
+
+pdflatex="pdflatex $filename.tex"
+bibtex="bibtex $filename"
+
+eval $pdflatex
+eval $bibtex
+eval $pdflatex
+eval $pdflatex
+
+rm $filename.{aux,bbl,blg,log,out}
