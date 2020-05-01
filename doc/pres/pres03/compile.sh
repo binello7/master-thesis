01883209912b74d791668ef645fb29b9691da7d7
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-05-01 17:42:33 +0200

    Update 'compile.sh' to compile 'Short_Abstract.tex' as well

--- doc/pres/pres03/compile.sh
+++ doc/pres/pres03/compile.sh
@@ -5,3 +5,6 @@ filename="03_Presentation"
 pdflatex -shell-escape $filename.tex
 rm -r _minted-*
 rm $filename.{aux,bcf,log,nav,out,run.xml,snm,toc}
+
+pdflatex Short_Abstract.tex
+rm Short_Abstract.{aux,log}
