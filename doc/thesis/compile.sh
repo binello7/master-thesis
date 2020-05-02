c9b66c635fe89a0a88f27430ba441616ade0551c
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-05-02 15:32:58 +0200

    Update 'compile.sh' to remove log files

--- doc/thesis/compile.sh
+++ doc/thesis/compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-pdflatex -shell-escape SR_MThesis_Emulation.tex
-biber SR_MThesis_Emulation
-pdflatex -shell-escape SR_MThesis_Emulation.tex
-pdflatex -shell-escape SR_MThesis_Emulation.tex
+filename="SR_MThesis_Emulation"
+
+pdflatex -shell-escape $filename.tex
+biber $filename
+pdflatex -shell-escape $filename.tex
+pdflatex -shell-escape $filename.tex
+
+rm -r _minted-*
+rm Appendices/Appendix.aux
+rm Chapters/*.aux
+rm $filename.{aux,bbl,bcf,blg,log,out,run.xml,toc}
